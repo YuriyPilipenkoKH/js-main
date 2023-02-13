94ba717c1609c37c0100a9528e695cf6ed48f7e3
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-13 16:27:29 +0200

    gallery

--- Module—7/js/01-gallery.js
+++ Module—7/js/01-gallery.js
@@ -1,3 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const imageGallery = document.querySelector('.gallery');
+
+const cardsMarkup = createImageGallery(galleryItems);
+imageGallery.innerHTML = cardsMarkup
+imageGallery.addEventListener('click',onPhotoClick)
+
+
+function createImageGallery(galleryItems) {
+    return galleryItems.map((gallery) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${gallery.preview}"
+                    data-source="${gallery.original}"
+                    alt="${gallery.description}"
+                />
+                </a>
+            </div>`;
+        })
+        .join('');
+    }
+ 
+function onPhotoClick(e) {
+
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}">
+    `)
+    instance.show();
+
+    imageGallery.addEventListener('keydown', (e) => {
+        if (e.code === "Escape") {
+            instance.close()
+        }
+    });
+};
 
