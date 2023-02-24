9ee9a96fa9cafeae0fc9bcf1e5e0b0d9f412bb4e
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-24 12:41:07 +0200

    storage

--- Module—8/js/feedback.js
+++ Module—8/js/feedback.js
@@ -1,9 +1,11 @@
-console.log(localStorage);
+// console.log(localStorage);
 
 const refs = {
     form: document.querySelector('form.feedback-form'),
     textarea: document.querySelector('form [name="message"]')
 }
+
+const STORAGE_KEY = 'feedback-msg'
 
 // console.log(refs.form);
 // console.log(refs.textarea);
@@ -11,10 +13,29 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit)
 refs.textarea.addEventListener('input', onTextInput)
 
-function onFormSubmit(e) {} 
+populateTextarea()
+
+function onFormSubmit(e) {
+    e.preventDefault()
+     
+        console.log('send form');
+
+    e.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
+} 
 
 function onTextInput(e) {
-    const value = e.currentTarget.value
+    const message = e.currentTarget.value
+    localStorage.setItem(STORAGE_KEY,message)
 
-    console.log(value);
+    console.log(message);
+}
+
+function populateTextarea(e) {
+    const saveMessage = localStorage.getItem(STORAGE_KEY)
+    if(saveMessage) {
+        console.log(saveMessage);
+
+        refs.textarea.value = saveMessage
+    }
 }
