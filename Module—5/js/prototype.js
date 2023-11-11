09c574c49e70944d24b736e95a80a08610f1f0d0
Yuriy Pilipenko <yurik2061@gmail.com>
2023-11-11 09:07:58 +0200

    proto

--- Module—5/js/prototype.js
+++ Module—5/js/prototype.js
@@ -2,8 +2,8 @@ const objC = {
     z:5,
 
 };
-console.log(objC);
-console.log(objC.hasOwnProperty('z'));
+// console.log(objC);
+// console.log(objC.hasOwnProperty('z'));
 
 const objB =Object.create(objC)
 objB.y = 2
@@ -21,3 +21,27 @@ objA.x =1
 // objA.z = 12
 // console.log(objA.z);
 // console.log(objA.hasOwnProperty('y')); 
+
+const user = {
+    name: "Jonny",
+    age: 45,
+    sayHello(){
+        console.log('Hello all')
+    },
+    sayMyName() {
+        console.log(this.name)
+    },
+    showMyAge(){
+        console.log(`My age ${this.age}`)
+    }
+}
+
+// console.log(user.sayMyName())
+console.log(user.showMyAge())
+
+const tommy = Object.create(user)
+tommy.name = 'Tommy'
+tommy.age = 28
+console.log( tommy.sayMyName())
+console.log(tommy.showMyAge())
+console.log(tommy)
